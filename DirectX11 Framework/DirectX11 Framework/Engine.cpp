682d6fbaad940f8e190ca898aa5cbfad40a6327c
Glennsssaaa <1904528@uad.ac.uk>
2023-08-17 15:18:51 +0100

    Implemented Keyboard Input

--- DirectX11 Framework/DirectX11 Framework/Engine.cpp
+++ DirectX11 Framework/DirectX11 Framework/Engine.cpp
@@ -7,3 +7,14 @@ bool Engine::Initialize(HINSTANCE hInstance, std::string window_title, std::stri
 bool Engine::ProcessMessages() {
 	return this->render_window.ProcessMessages();
 }
+
+void Engine::Update() {
+	while(!keyboard.CharBufferIsEmpty()){
+		unsigned char ch = keyboard.ReadChar();
+	}
+
+	while (!keyboard.KeyBufferIsEmpty()) {
+		KeyboardEvent kbe = keyboard.ReadKey();
+		unsigned char keycode = kbe.GetKeyCode();
+	}
+}
