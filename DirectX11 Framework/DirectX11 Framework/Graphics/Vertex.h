4d81df911b268ff6a676f8135cefe318f5ed9291
Glennsssaaa <1904528@uad.ac.uk>
2023-08-18 11:39:26 +0100

    Implemented Depth Stencil

--- DirectX11 Framework/DirectX11 Framework/Graphics/Vertex.h
+++ DirectX11 Framework/DirectX11 Framework/Graphics/Vertex.h
@@ -4,9 +4,9 @@
 struct Vertex
 {
 	Vertex(){}
-	Vertex(float x, float y, float r, float g, float b)
-		:pos(x,y), color(r,g,b){}
-	DirectX::XMFLOAT2 pos;
+	Vertex(float x, float y, float z, float r, float g, float b)
+		:pos(x,y,z), color(r,g,b){}
+	DirectX::XMFLOAT3 pos;
 	DirectX::XMFLOAT3 color;
 };
 
